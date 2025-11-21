06f76256f678049b3438167ba58d1695412adda6
RikhilPatta <prikhilpatta@gmail.com>
2025-11-21 12:25:04 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express=require("express");
+const path =require("path");
+
+const app =express();
+const server=require("http").createServer(app);
+
+const io = require("socket.io")(server);
+
+app.use(express.static(path.join(__dirname+"/public")));
+
+io.on("connection", function(socket){
+    socket.on("newuser",function(username){
+        socket.broadcast.emit("update",username + " joined the class");
+    });
+    socket.on("exituser",function(username){
+        socket.broadcast.emit("update",username + " left the class");
+    });
+    socket.on("chat",function(message){
+        socket.broadcast.emit("chat",message);
+    });
+});
+server.listen(3001, "0.0.0.0", () => {
+     console.log("Server running on port 3001");
+});
+//server.listen(5000);
